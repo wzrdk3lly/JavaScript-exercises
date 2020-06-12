d86899344dc96b9fac3734d063769cccdf6183c7
miles <dkmk1149@gmail.com>
2020-06-12 01:17:54 -0400

    updated exercises

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,4 +1,27 @@
-const caesar = function() {
+const caesar = function(text, num) {
+    let encrypt = '';
+    for(let i = 0; i < text.length; i++){
+        if(containsPunc(text.charAt(i))){
+         encrypt += text.charAt(i);
+        }
+        else if(num > 0){
+            // while(containsPunc(String.fromCharCode(text.charCodeAt(i)+num))){
+            //     num++;
+            // }
+          encrypt += String.fromCharCode(text.charCodeAt(i)+num);
+        }
+        else if(num < 0){
+            encrypt += String.fromCharCode(text.charCodeAt(i)-num);
+          }
+    }
+    return encrypt;
+}
+
+function containsPunc(text){
+  return text === ',' ||
+         text === '.' ||
+         text === ' ' ||
+         text === '!';
 
 }
 
