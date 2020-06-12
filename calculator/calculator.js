65c99c549adeb8dfc8f41ce74882fea3158c2b68
miles <dkmk1149@gmail.com>
2020-06-12 01:24:48 -0400

    latest problems

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,27 +1,48 @@
-function add () {
+function add (num1, num2) {
+	return num1 + num2;
+}
+
+function subtract (num1,num2) {
+	return num1 - num2;
 	
 }
 
-function subtract () {
-	
+function sum (arr) {
+	return arr.reduce(function (total, item) {
+		return total + item;
+	},0)
 }
 
-function sum () {
-	
+function multiply (arr) {
+	if(arr[0] === undefined){
+		return 0;
+	}
+	return arr.reduce((product, curr) => {
+		return product * curr;
+	});
 }
 
-function multiply () {
-	
+function power(base,power) {
+	let final = 1;
+	for(let i = 0; i<power; i++){
+		final *= base;
+	}
+	return final;
 }
+// Could have used math.pow()  :( so simple
 
-function power() {
-	
+function factorial(num) {
+	let factorial = 1;
+	if(num === 0){
+		return factorial;
+	}
+
+	for(let i = 0; i < num; i++){
+		factorial *= (num - i);
+	}
+	return factorial;
+
 }
-
-function factorial() {
-	
-}
-
 module.exports = {
 	add,
 	subtract,
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function() {
-
+const palindromes = function(input) {
+    let cleanInput = input.toUpperCase().split("").filter(getOnlyLetters);
+    let reverseArr = [];
+  
+    for(let i = cleanInput.length -1; i > -1; i--){
+      reverseArr.push(cleanInput[i]);
+    }
+    
+    let str1 = cleanInput.toString();
+    let str2 = reverseArr.toString();
+  
+    return str1 === str2;
 }
 
+function getOnlyLetters(text){
+    return text !== " " && 
+           text !== "!" &&
+           text !== "." &&
+           text !== "," ;
+  }
 module.exports = palindromes
