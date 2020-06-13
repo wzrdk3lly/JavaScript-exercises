87ea7efdb689f13f435822c792bf542c9d7a81ce
miles <dkmk1149@gmail.com>
2020-06-13 13:37:22 -0400

    Fibonnaci completed

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
-}
-
+const fibonacci = function(num) {
+    let input = +(num);
+    let fib = 0;
+    let arrFib = [1,1];
+  
+    if(input < 0){
+    return 'OOPS';
+    }
+    else{
+    for( let i = 0; i < 100; i++){
+      arrFib.push(arrFib[i]+arrFib[i +1]);
+    }
+    }
+    
+    return fib = arrFib[input -1];
+  }
 module.exports = fibonacci
