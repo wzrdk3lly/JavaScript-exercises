ed111291ff1f01036e356b313c7b61ea93e6e517
miles <dkmk1149@gmail.com>
2020-06-13 15:28:23 -0400

    get the titles completed

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    const bookTitles = obj.map(book => book.title);
+    return bookTitles;
 }
 
 module.exports = getTheTitles;
